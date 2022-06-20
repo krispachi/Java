9d33f05f75d2e380b2d87cfbef88ec5a743a4b94
Krisna <93766212+krispachi@users.noreply.github.com>
2022-06-20 19:21:05 +0800

    Add files via upload

---
+++ Tutor.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Tutor {
+    public static void main(String[] args) {
+
+        Scanner inputUser = new Scanner(System.in);
+
+        System.out.print("Ketik NAMAMU!!! : ");
+        String input = inputUser.next();
+
+        System.out.println("halo " + input);
+
+    }
+}
